5c94423b302b7810c20f2e977ef1b5f7945dc392
Bob Chunghee Lee <bob@fantom.foundation>
2018-05-19 11:09:35 +0900

    Make sample assembly files more complicated

--- assembler/sample/sample.asm
+++ assembler/sample/sample.asm
@@ -3,13 +3,13 @@ nop
 nop
 nop
 nop
-subi r255 fa00h
+subi r255 r2
 jmp_eq r15 r14 ff0eh
 mov r4 r5
 addi r30 r10
-addi r255 0h
-addi r10 ff00ffh
-muli r55 f0ffh
+addi r255 r2
+addi r10 r4
+muli r55 r9
 jmp_ne r255 r254 f023h
 mov r10 7ch
 mov r255 r254
@@ -25,9 +25,8 @@ modi r13 r23
 jmp_ge r13 r9 bc74h
 jmp_gt r13 r9 bc74h
 mov r255 ff86fffbe7dcffcch
-subi r254 ffh
+subi r254 r10
 addi r9 r12
 addi r12 r13
 jmp_ne r13 r9 bc74h
 jmp_ne r4 r255 ff87h
-mov r10 fah
